bb93706de8119a8335925821d0fa9511a9e83178
ashiato45 <katiiijp@gmail.com>
2018-12-15 15:21:37 +0900

    added initializing file

---
+++ db.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `messages`(
+       `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+       `username` VARCHAR(64) NULL,
+       `message` VARCHAR(140) NULL,
+       `created` DATE NULL
+);
---
+++ memo.md
@@ -0,0 +1,3 @@
+## To initialize the database
+`sqlite3 -init db.sql test.db < /dev/null`
+Input devnull not to start interactive mode.
