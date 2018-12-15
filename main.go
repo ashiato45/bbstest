681cd8a04dfe2f81d3307d0b85d55d56226a5d86
ashiato45 <katiiijp@gmail.com>
2018-12-16 02:24:07 +0900

    ok

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
     "crypto/md5"
     "strconv"
     "io"
+    "database/sql"
+    _ "github.com/mattn/go-sqlite3"
 )
 
 func putData(w http.ResponseWriter, r * http.Request){
@@ -22,24 +24,77 @@ func putData(w http.ResponseWriter, r * http.Request){
         fmt.Println("key:", k)
         fmt.Println("val:", strings.Join(v, ""))
     }
+
+    
+}
+
+func writeMessageOnDB(w http.ResponseWriter, r * http.Request, db *sql.DB){
+    stmt, err := db.Prepare("INSERT INTO messages(username, message, created) values(?,?,?)")
+    checkErr(err, "preparing")
+
+
+    loc, err := time.LoadLocation("Asia/Tokyo")
+    checkErr(err, "loading location")
+    username := r.Form.Get("username")
+    message := r.Form.Get("message")
+    created := time.Now().In(loc)
+
+    res, err := stmt.Exec(username, message, created)
+    checkErr(err, "executing")
+    _ = res
+}
+
+func checkErr(err error, mes string){
+    if err != nil{
+        println("[message] %v", mes)
+        panic(err)
+    }
+}
+
+func readDB(db *sql.DB) ([](map[string]string)){
+    result := make([](map[string]string), 0)
+    rows, err := db.Query("SELECT * FROM messages")
+    for rows.Next(){
+        var uid int
+        var username string
+        var message string
+        var date string
+        err = rows.Scan(&uid, &username, &message, &date)
+        checkErr(err, "reading")
+        result = append(result, map[string]string {"username": username, "message": message, "date": date})
+    }
+    return result
 }
 
 
 func sayRoot(w http.ResponseWriter, r *http.Request){
+    db, err := sql.Open("sqlite3", "./test.db")
+    checkErr(err, "reading database")
+
+    
     if r.Method == "GET"{
     }else{
         putData(w, r)
+        writeMessageOnDB(w, r, db)
+
     }
     crutime := time.Now().Unix()
     h := md5.New()
     io.WriteString(h, strconv.FormatInt(crutime, 10))
-    token := fmt.Sprintf("%x", h.Sum(nil))
-    
+    params := map[string]interface{} {}
+    params["token"] = fmt.Sprintf("%x", h.Sum(nil))
+
+    //row1 := map[string]string{"username": "tarou", "message": "happy!", "date": "2019/1/1"}
+    //rows := [](map[string]string){row1}
+    rows := readDB(db)
+    params["rows"] = rows
+
     t, _ := template.ParseFiles("test.gtpl")
-    t.Execute(w, token)
+    t.Execute(w, params)
 }
 
 func main(){
+    println("hoge")
     http.HandleFunc("/", sayRoot)
     err := http.ListenAndServe(":9090", nil)
     if err != nil{
--- test.gtpl
+++ test.gtpl
@@ -6,8 +6,23 @@
 <form action="/" method="post">
     Username: <input type="text" name="username">
     Message: <input type="text" name="message">
-    <input type="hidden" name="token" value="{{.}}">
+    <input type="hidden" name="token" value="{{.token}}">
     <input type="submit" value="Write">
 </form>
+<table border="1">
+  <tr>
+    <th>Username</th>
+    <th>Message</th>
+    <th>Date</th>
+  </tr>
+  {{range .rows}}
+  <tr>
+    <td>{{.username}}</td>
+    <td>{{.message}}</td>
+    <td>{{.date}}</td>    
+  </tr>
+  {{end}}
+
+</table>
 </body>
 </html>
